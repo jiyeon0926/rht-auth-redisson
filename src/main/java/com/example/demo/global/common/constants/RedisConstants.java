94ddc89499ff93ca516d7c1a0107956efd8c33ea
jiyeon0926 <yeoni9094@naver.com>
2025-05-22 10:33:50 +0900

    feat: RedissonConfig

---
+++ src/main/java/com/example/demo/global/common/constants/RedisConstants.java
@@ -0,0 +1,9 @@
+package com.example.demo.global.common.constants;
+
+public final class RedisConstants {
+
+    // 인스턴스 생성 방지
+    private RedisConstants() {}
+
+    public static final String REDIS_URL_PREFIX = "redis://";
+}
--- src/main/java/com/example/demo/global/config/RedissonConfig.java
+++ src/main/java/com/example/demo/global/config/RedissonConfig.java
@@ -1,5 +1,9 @@
 package com.example.demo.global.config;
 
+import com.example.demo.global.common.constants.RedisConstants;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +12,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
-public class RedisConfig {
+public class RedissonConfig {
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -19,5 +23,16 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(RedisConstants.REDIS_URL_PREFIX + host + ":" + port);
+
+        // JSON 형식으로 직렬화 및 역직렬화
+        config.setCodec(new org.redisson.codec.JsonJacksonCodec());
+
+        return Redisson.create(config);
     }
 }
