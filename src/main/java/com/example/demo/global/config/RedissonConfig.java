75e6b5fa8dbb19ea6d3b090cc37a79c382c702b1
jiyeon0926 <yeoni9094@naver.com>
2025-05-22 11:33:56 +0900

    fix: RedisConnectionFactory 삭제
    - Redisson은 독립적인 클라이언트 라이브러리이기 때문에 Redisson 의존성만 추가된 상태
    - RedisConnectionFactory 필요없음

--- src/main/java/com/example/demo/global/config/RedissonConfig.java
+++ src/main/java/com/example/demo/global/config/RedissonConfig.java
@@ -7,9 +7,6 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 public class RedissonConfig {
@@ -19,11 +16,6 @@ public class RedissonConfig {
 
     @Value("${spring.data.redis.port}")
     private int port;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
-    }
 
     @Bean
     public RedissonClient redissonClient() {
